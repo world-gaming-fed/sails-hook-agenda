07968ed46b6759139714173a94441c62b6272ee5
Damien Berseron (Hikkyu) <hikkyu@wgf.gg>
2016-01-26 10:58:07 +0100

    some cleaning

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 'use strict';
 
-module.exports = function(app) {
+module.exports = function(sails) {
 
   return {
     defaults: require('./lib/defaults'),
 
-    configure: require('./lib/configure')(app),
+    configure: require('./lib/configure')(sails),
 
-    initialize: require('./lib/initialize')(app)
+    initialize: require('./lib/initialize')(sails)
   };
 };
